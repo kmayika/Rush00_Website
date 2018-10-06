005210e8c1be2bd0e85e49859afb0f358c58ebb1
Kwezi Mayikana <https://github.com/kmayika>
2018-10-07 00:13:07 +0200

    not done

---
+++ rush00/template.php
@@ -0,0 +1,35 @@
+
+<html>
+    <head>
+    <title><?php echo $title ;?></title>
+    <link rel="stylesheet" href="style_sheet.css">
+    </head>
+<body>
+    <div id="wholepage">
+        <div id="banner">
+            <h1 id="store_name">UVOART ONLINE</h1>
+
+        </div>
+        <nav id="navigation">
+            <ul id="nav">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="#">Art</a></li>
+                <li><a href="#">Shop</a></li>
+                <li><a href="#">About</a></li>
+
+        </nav>
+        <div id="content_area">
+            <?php 
+                echo $content; 
+            ?>
+        </div>
+        <div id="sidebar">
+        
+        </div>
+
+        <footer>
+            <p>All rights reserved</p>
+        </footer>
+    </div>
+</body>
+</html>
